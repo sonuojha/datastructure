39141c2695b0b1db099a8d71e9b5fb6b041e2a19
aojha <aojha@paypal.com>
2017-05-23 11:29:13 +0530

    added master branch

---
+++ src/LinkList.java
@@ -0,0 +1,15 @@
+/**
+ * Created by aojha on 5/23/17.
+ */
+
+class Node{
+    int data;
+    Node next;
+};
+
+
+public class LinkList {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
